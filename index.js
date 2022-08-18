d6ff1754432f47cdd62635633ff787f7ea6ea643
Victorious <new@mail.com>
2022-08-18 18:10:58 +0300

    Completed Functions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`)
+}
+
+function mondayWork(act = "go to the office") {
+    return `This Monday, I will ${act}.`
+}
+
+function wrapAdjective(msg) {
+    return function(adj) {
+        return `You are ${msg}${adj}${msg}!`
+
+    }
+}
+wrapAdjective("!")("a dedicated programmer")
